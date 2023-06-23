53cc4968e4f1da6b84d3a123ff501123a30a4964
Jamiro Ferrara <ferrarajamiro@hotmail.com>
2023-06-23 15:16:31 +0200

    Initial Commit

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+macro_rules! sh {
+    ($var:expr) => {
+        let err = format!("Failed to execute {}", $var);
+        let output = Command::new("sh").arg("-c").arg($var).output().expect(&err);
+
+        if output.status.success() {
+            // Print the stdout if the command executed successfully
+            let stdout = String::from_utf8_lossy(&output.stdout);
+            println!("Command output:\n{}", stdout);
+        } else {
+            // Print the stderr if the command failed
+            let stderr = String::from_utf8_lossy(&output.stderr);
+            println!("Command failed:\n{}", stderr);
+        }
+    };
+}
